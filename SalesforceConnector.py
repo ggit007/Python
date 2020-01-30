e1aad64c0d7072277078b771fc6a94f2aacfc044
ggit007 <57431105+ggit007@users.noreply.github.com>
2020-01-30 13:23:03 +0530

    Update SalesforceConnector.py

--- SalesforceConnector.py
+++ SalesforceConnector.py
@@ -10,30 +10,34 @@ class SalesforceConnector:
     password = 'Gaurav12@'
     
     auth_uri = 'https://login.salesforce.com/services/oauth2/token'
-    
-    response = req.post(auth_uri, data = {
-                        'client_id' : client_id,
-                        'client_secret' : client_secret,
-                        'grant_type' : 'password',
-                        'username' : sfdc_username,
-                        'password' : password
-    })
-    
-    #print('response*******',help(response))
-    #print('status code******* ',response.status_code)
-    if response.status_code == 200:
-        json_response = response.json()
-        print('json_response***** ',json_response)
-        access_token = json_response['access_token']
-        instance_url =  json_response['instance_url']
-        token_type = json_response['token_type']    #Bearer
-        auth = {'Authorization':token_type+' '+access_token}
+    try:
+        response = req.post(auth_uri, data = {
+                            'client_id' : client_id,
+                            'client_secret' : client_secret,
+                            'grant_type' : 'password',
+                            'username' : sfdc_username,
+                            'password' : password
+        })
         
-        url = instance_url + '/services/data/v47.0/query/?q=SELECT+Name+FROM+Account LIMIT 10'
-        data_response = req.get(url, headers=auth)
-        data_reponse_json = data_response.json()
-        #print('data_response_status@@@@@@@@@@@ ',data_response.json())
-        #print('data_response@@@@@@@@@@@',data_response.json()['totalSize'])
-        #print('data_response@@@@@@@@@@@',data_response.json()['records'])
-        for each in data_reponse_json['records']:
-            print('%%%%% ',each['Name'])
+        #print('response*******',help(response))
+        #print('status code******* ',response.status_code)
+        if response.status_code == 200:
+            json_response = response.json()
+            print('json_response***** ',json_response)
+            access_token = json_response['access_token']
+            instance_url =  json_response['instance_url']
+            token_type = json_response['token_type']    #Bearer
+            auth = {'Authorization':token_type+' '+access_token}
+            
+            url = instance_url + '/services/data/v47.0/query/?q=SELECT+Name+FROM+Account LIMIT 10'
+            data_response = req.get(url, headers=auth)
+            data_reponse_json = data_response.json()
+            #print('data_response_status@@@@@@@@@@@ ',data_response.json())
+            #print('data_response@@@@@@@@@@@',data_response.json()['totalSize'])
+            #print('data_response@@@@@@@@@@@',data_response.json()['records'])
+            for each in data_reponse_json['records']:
+                print('%%%%% ',each['Name'])
+        else:
+            print('Error code: ',response.status_code)
+    except:
+        raise Exception('Error code: ',response.status_code)
